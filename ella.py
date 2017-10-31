aa569741c0c6ffc2e620455e56720ef01af78e30
Sam Hart <criswellious@gmail.com>
2017-10-31 12:56:01 -0400

    lots of crap on ella

--- ella.py
+++ ella.py
@@ -5,6 +5,8 @@ import re
 import spacy
 import sys
 import os
+import argparse
+import json
 
 nlp = spacy.load("en")
 
@@ -16,4 +18,82 @@ class POSifiedText(markovify.Text):
         sentence = " ".join(word.split("::")[0] for word in words)
         return sentence
 
+parser = argparse.ArgumentParser()
+parser.add_argument("config", help="The JSON config file to load.")
 
+args = parser.parse_args()
+
+config = {}
+with open(args.config, 'r') as f:
+    config = json.load(f)
+
+bad_config = False
+stanzas_missing = []
+
+# Config handling and setup
+def check_stanza(key, description):
+    global bad_config, stanzas_missing
+    if not key in config:
+        bad_config = True
+        stanzas_missing.append({'name':key, 'description':description})
+
+check_stanza("main_corpus_source", "The path to the main corpus directory")
+check_stanza("main_corpus_json", "The path to the main corpus JSON")
+check_stanza("ad_corpora_json_dir", "The path of the ad corpora JSON files")
+check_stanza("ad_corpora", "The collection of ad copora")
+
+if bad_config:
+    print("Error parsing config file!\n")
+    print("The following stanzas were missing from the config file:\n")
+    for st in stanzas_missing:
+        print("\tstanza: '{0}'".format(st['name']))
+        print("\t{0}\n".format(st['description']))
+    sys.exit(1)
+
+def load_corpus(json_file, fullpath):
+    model = None
+    if os.path.isfile(json_file):
+        try:
+            with open(json_file, 'r') as f:
+                j = json.load(f)
+            model = markovify.Text.from_json(j)
+        except:
+            print("Error parsing json file: {0}".format(json_file))
+            print("Trying to load corpus, and JSON file invalid!")
+            sys.exit(1)
+    if model is None:
+        # Okay, we load it from the directory
+        for (dirpath, _, filenames) in os.walk(fullpath):
+            for filename in filenames:
+                with open(os.path.join(dirpath, filename)) as f:
+                    m = POSifiedText(f, retain_original=False)
+                if model:
+                    model = markovify.combine(models=[model, m])
+                else:
+                    model = m
+
+    return model
+
+def save_corpus(json_file, model):
+    j = model.to_json()
+    with open(json_file, 'w') as f:
+        f.write(j)
+
+# Load corpora
+#--------------
+main_model = load_corpus(config['main_corpus_json'],config['main_corpus_source'])
+ad_corpora = {}
+for k in config['ad_corpora']:
+    n = k['name']
+    jf = os.path.join(config['ad_corpora_json_dir'], '{0}.json'.format(n))
+    fp = k['source']
+    model = load_corpus(jf, fp)
+    ad_corpora[n] = model
+
+# Save corpora
+#--------------
+save_corpus(config['main_corpus_json'], main_model)
+for k in config['ad_corpora']:
+    n = k['name']
+    jf = os.path.join(config['ad_corpora_json_dir'], '{0}.json'.format(n))
+    save_corpus(jf, ad_corpora[n])
